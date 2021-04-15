01514d891132294560157e5aa37f7128ee95dfa7
Ryan Desnick <ryandesnick@gmail.com>
2021-04-15 02:22:59 -0400

    question assembly

--- dist/index.2142d36c.js
+++ dist/index.2142d36c.js
@@ -442,7 +442,60 @@ id) /*: string*/
 }
 
 },{}],"3L8AI":[function(require,module,exports) {
-console.log("connected lmao");
+const question = document.querySelector("#question");
+const choices = Array.from(document.querySelectorAll(".choice-text"));
+const progressText = document.querySelector("#progress-text");
+const scoreText = document.querySelector("#score");
+const progressBarFull = document.querySelector("#progress-bar-full");
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let avaliableQuestions = {};
+
+let questions = [
+  {
+    question: "Who is not bald?",
+    choice1: "Henriques",
+    choice2: "Whalen",
+    choice3: "Rerick",
+    choice4: "Erlenwein",
+    answer: Rerick,
+  },
+  {
+    question: "Which restaurant has not closed down?",
+    choice1: "Blaze Pizza",
+    choice2: "KFC",
+    choice3: "Five Guys",
+    choice4: "TexMex",
+    answer: TexMex,
+  },
+  {
+    question: "Which staircase should you not go up during lunchtime?",
+    choice1: "H",
+    choice2: "I",
+    choice3: "C",
+    choice4: "F",
+    answer: I,
+  },
+  {
+    question: "How many minutes apart does the 79 (usually) come?",
+    choice1: "Eight",
+    choice2: "Twenty",
+    choice3: "Fifteen",
+    choice4: "Five",
+    answer: Eight,
+  },
+  {
+    question: "Which MSIT sport is actually a cult?",
+    choice1: "Track",
+    choice2: "Ping-Pong",
+    choice3: "Football",
+    choice4: "Swimming",
+    answer: Track,
+  },
+];
 
 },{}]},["21c8X","3L8AI"], "3L8AI", "parcelRequire3da0")
 
