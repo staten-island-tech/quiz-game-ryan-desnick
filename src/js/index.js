3ec8a4e376688ef08765ca099ccce11cf8c325ec
Ryan Desnick <ryandesnick@gmail.com>
2021-05-11 03:36:04 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
@@ -17,8 +18,8 @@ function startGame() {
 }
 
 function setNextQuestion() {
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
   resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
@@ -38,11 +39,32 @@ function showQuestion(question) {
 function resetState() {
   nextButton.classList.add("hide");
   while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild;
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
 }
 
-function selectAnswer(e) {}
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
 
 const questions = [
   {
