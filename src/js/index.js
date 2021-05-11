a6bfab05f7d6cf978658a8e3675c0a4f31f524e9
Ryan Desnick <ryandesnick@gmail.com>
2021-05-11 03:25:34 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,57 @@
 const startButton = document.getElementById("start-btn");
 const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener("click", startGame);
 
 function startGame() {
   console.log("started");
   startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
   questionContainerElement.classList.remove("hide");
   setNextQuestion();
 }
 
-function setNextQuestion() {}
+function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+  resetState();
+}
 
-function selectAnswer() {}
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild;
+  }
+}
+
+function selectAnswer(e) {}
 
 const questions = [
   {
-    question: "",
+    question: "Who is not bald?",
+    answers: [
+      { text: "Rerick", correct: true },
+      { text: "Henriques", correct: false },
+      { text: "Whalen", correct: false },
+      { text: "Erlenwein", correct: false },
+    ],
   },
 ];
