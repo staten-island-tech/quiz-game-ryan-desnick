5f3fe10dda49ae4979f1b29003ef4e8db0c4fa36
Ryan Desnick <ryandesnick@gmail.com>
2021-05-11 03:36:08 -0400

    Update index.2142d36c.js

--- dist/index.2142d36c.js
+++ dist/index.2142d36c.js
@@ -140,9 +140,9 @@
       this[globalName] = mainExports;
     }
   }
-})({"747qG":[function(require,module,exports) {
+})({"21c8X":[function(require,module,exports) {
 var HMR_HOST = null;
-var HMR_PORT = 56915;
+var HMR_PORT = 1234;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d751713988987e9331980363e24189ce";
 module.bundle.HMR_BUNDLE_ID = "062e9a7565ca912a5f7d6b832142d36c";
@@ -443,6 +443,7 @@ id) /*: string*/
 
 },{}],"3L8AI":[function(require,module,exports) {
 const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
@@ -461,8 +462,8 @@ function startGame() {
 }
 
 function setNextQuestion() {
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
   resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
@@ -482,11 +483,32 @@ function showQuestion(question) {
 function resetState() {
   nextButton.classList.add("hide");
   while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild;
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
 }
 
-function selectAnswer(e) {}
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
 
 const questions = [
   {
@@ -500,6 +522,6 @@ const questions = [
   },
 ];
 
-},{}]},["747qG","3L8AI"], "3L8AI", "parcelRequire3da0")
+},{}]},["21c8X","3L8AI"], "3L8AI", "parcelRequire3da0")
 
 //# sourceMappingURL=index.2142d36c.js.map
