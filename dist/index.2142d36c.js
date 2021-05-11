8846a5adfd87026ba1dc61a7aa43262e50d9f650
Ryan Desnick <ryandesnick@gmail.com>
2021-05-11 03:46:03 -0400

    Update index.2142d36c.js

--- dist/index.2142d36c.js
+++ dist/index.2142d36c.js
@@ -451,6 +451,10 @@ const answerButtonsElement = document.getElementById("answer-buttons");
 let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
   console.log("started");
@@ -494,6 +498,7 @@ function selectAnswer(e) {
   Array.from(answerButtonsElement.children).forEach((button) => {
     setStatusClass(button, button.dataset.correct);
   });
+  nextButton.classList.remove("hide");
 }
 
 function setStatusClass(element, correct) {
